150c9a0de0be4c98cd7b6dc6d8fdd7fb929d5ef9
davemmWeb <davemm.ku@gmail.com>
2023-03-06 00:08:47 -0500

    Updates

---
+++ precache-manifest.a664fad786c3c2702c50bb4f63c296f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0443e77b43bccec6510",
+    "url": "/project_typescript/static/css/main.170961d9.chunk.css"
+  },
+  {
+    "revision": "e0443e77b43bccec6510",
+    "url": "/project_typescript/static/js/main.e0443e77.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/project_typescript/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "6ad45a30032a91a8ea88",
+    "url": "/project_typescript/static/js/2.6ad45a30.chunk.js"
+  },
+  {
+    "revision": "e173132bfbfddbbd8300",
+    "url": "/project_typescript/static/js/runtime~main.e173132b.js"
+  },
+  {
+    "revision": "c1f81144897fc5acffc67de8febd9690",
+    "url": "/project_typescript/static/media/Saiyan-Sans.c1f81144.ttf"
+  },
+  {
+    "revision": "964d79cc2ce11b179031b237f3feda77",
+    "url": "/project_typescript/static/media/kame- house.964d79cc.jfif"
+  },
+  {
+    "revision": "a9ffcb73834da90a7f9e2dad469c6d97",
+    "url": "/project_typescript/index.html"
+  }
+];
