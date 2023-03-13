f27b07e4fdcd93f169d2cf146723fcdfed177e19
davemmWeb <davemm.ku@gmail.com>
2023-03-12 23:48:58 -0500

    Updates

---
+++ precache-manifest.85597836b031dd592093133e7cb99f2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c031e9bde7ea4a72adc",
+    "url": "/project_typescript/static/css/main.efa9453e.chunk.css"
+  },
+  {
+    "revision": "2c031e9bde7ea4a72adc",
+    "url": "/project_typescript/static/js/main.2c031e9b.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/project_typescript/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "73631bb8a79bdb2fdbab",
+    "url": "/project_typescript/static/js/2.73631bb8.chunk.js"
+  },
+  {
+    "revision": "552a8f09b2f73002bc07",
+    "url": "/project_typescript/static/js/runtime~main.552a8f09.js"
+  },
+  {
+    "revision": "964d79cc2ce11b179031b237f3feda77",
+    "url": "/project_typescript/static/media/kame- house.964d79cc.jfif"
+  },
+  {
+    "revision": "c1f81144897fc5acffc67de8febd9690",
+    "url": "/project_typescript/static/media/Saiyan-Sans.c1f81144.ttf"
+  },
+  {
+    "revision": "db7de750d5a1068f07c6e9d42cbeaaee",
+    "url": "/project_typescript/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/project_typescript/precache-manifest.7bcd5b2e19934e58b316f3a770d10b88.js"
+  "/project_typescript/precache-manifest.85597836b031dd592093133e7cb99f2b.js"
 );
 
 workbox.clientsClaim();
